9362bfa59aed9c108a04576866060f01d732361f
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-02-29 08:57:51 +0000

    adicionar janela HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class HelloWorld extends JFrame {
+    private JPanel panel1;
+    private JLabel lblMensagem;
+
+    public HelloWorld(){
+        setContentPane(panel1);
+        pack();
+    }
+    public static void main(String[] args) {
+        new HelloWorld().setVisible(true);
+    }
+}
